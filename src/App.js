6bd187d1163f8ee0a87644f7cdba2502e3cf51d6
chrisnyman <chris@gradconnection.com>
2017-06-13 13:59:19 +0200

    Setup app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>gc-notification</h2>
+        <hr/>
+
+        <section>
+          <h4>Notification Banner</h4>
+
+          <p>Banner that appears at the top of the page</p>
+
+          <div>
+            <button
+              className="success-btn btn">Success</button>
+            <button
+              className="info-btn btn">Information</button>
+            <button
+              className="warning-btn btn">Warning</button>
+            <button
+              className="danger-btn btn">Danger</button>
+          </div>
+
+        </section>
+
+        <section>
+          <h4>Toast Notification</h4>
+
+        <p>Small animation that appears in the top right corner of the screen.</p>
+
+          <div>
+            <button
+              className="success-btn btn">Success</button>
+            <button
+              className="info-btn btn">Information</button>
+            <button
+              className="warning-btn btn">Warning</button>
+            <button
+              className="danger-btn btn">Danger</button>
+          </div>
+        </section>
       </div>
     );
   }
