c4dcfb0093b2178b290ef3f69b95ea586d499176
chrisnyman <chris@gradconnection.com>
2017-06-14 08:31:22 +0200

    Add manual closing functionality

--- src/components/GCNotification.jsx
+++ src/components/GCNotification.jsx
@@ -4,25 +4,42 @@ import CloseIcon from './CloseIcon';
 import './GCNotification.css';
 
 class GCNotification extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      isOpen: true,
+    }
+  }
+
+  close() {
+    this.setState({isOpen: false});
+  }
 
   render() {
-    return (
-      <div className="GCNotification GCNotification__toast GCNotification__success">
-        <p className="GCNotification--msg">
-          {this.props.message}
-        </p>
+    if (this.state.isOpen) {
+      return (
+        <div className="GCNotification GCNotification__toast GCNotification__success">
+          <p className="GCNotification--msg">
+            {this.props.message}
+          </p>
 
-        <div className="GCNotification--close">
-          <CloseIcon
-            color="#000000"
-            size="12px"
-          />
+          <div className="GCNotification--close"
+            onClick={() => this.close()}>
+            <CloseIcon
+              color="#000000"
+              size="12px"
+            />
+          </div>
+
+          <div className="GCNotification--timer"/>
+
         </div>
+      );
+    } else {
+      return null;
+    }
 
-        <div className="GCNotification--timer"/>
-
-      </div>
-    );
   }
 }
 
