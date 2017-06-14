49d70a0f8450e7c984595bc2f3110af917a370dd
chrisnyman <chris@gradconnection.com>
2017-06-14 13:00:34 +0200

    Create NotificationContainer

---
+++ src/container/GCNotificationContainer.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import GCNotification from '../components/GCNotification';
+
+class GCNotificationContainer extends Component {
+
+}
+
+export default GCNotificationContainer;
